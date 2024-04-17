3433b9be5d01251b2a3ead06f54c5c70ce0d07cb
ram7909 <ramcharanmain07@gmail.com>
2024-04-17 16:58:11 +0530

    Javascript Day-1

---
+++ Day-28_JS_Intro/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Console");
+console.log("We Are Learning Java Script");
+console.log("Right Now");
+let n1=50;
+let n2=50;
+console.log(n1+n2);
+const n3=50;
+const n4=10;
+console.log(n3+n4);
+var n5=50;
+var n6=30;
+console.log(n5+n6);
