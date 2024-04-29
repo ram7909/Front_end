58007037790305e1892f557e3e6cfd1ac4a97607
ram7909 <ramcharanmain07@gmail.com>
2024-04-29 17:23:51 +0530

    Yt Player

---
+++ Day-36_JS_9_Array/index.js
@@ -0,0 +1,21 @@
+// let list = [1,4,0,9,11,8,10];
+// let high = list[0];
+// let sh;
+// for(let i=0;i<list.length;i++){
+//     if(high<list[i]){
+//         sh = high;
+//         high = list[i];
+//     }
+//     else if(list[i]>sh){
+//         sh=list[i]
+//     }
+// }
+// console.log(sh);
+
+
+//split array
+let arr = [12,23,45,67,4,56,90];
+let m = arr.length%2!=0 ? (arr.length+1)/2 : (arr.length)/2;
+console.log(m)
+
+
