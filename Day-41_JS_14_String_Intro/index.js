337d4a3568449c954bd675ea718710142049d38b
ram7909 <ramcharanmain07@gmail.com>
2024-05-07 17:20:07 +0530

    String Upper and Lower case Method

---
+++ Day-41_JS_14_String_Intro/index.js
@@ -0,0 +1,20 @@
+// const n = '10';
+// console.log(typeof(n));
+// const str = "superman"
+// const str = "SUPERMAN"
+// console.log(str);
+
+
+//upercase
+// const temp = str.toUpperCase();
+// console.log(temp);
+
+//lowercase
+// const temp = str.toLowerCase();
+// console.log(temp);
+
+//trim
+const str = "        superman sharma ji    ";
+const temp = str.trim();
+console.log(temp);
+
