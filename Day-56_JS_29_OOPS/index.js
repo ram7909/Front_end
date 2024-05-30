5b787be9f6ea5c0e1dfdcb1c63305de8b671f1a8
ram7909 <ramcharanmain07@gmail.com>
2024-05-30 16:47:58 +0530

    OOPS Intro

---
+++ Day-56_JS_29_OOPS/index.js
@@ -0,0 +1,13 @@
+//Classes => blueprint of object
+
+//Object => real world entity
+
+//Constructor => 
+
+//Encapsulation => Data Hiding
+
+//Inheritance => Aquaring the parent property
+
+//Polymorphism => one entity can play multiple role based on suituation
+
+//Data Abstraction => showing only the functionality and hiding implementation details
