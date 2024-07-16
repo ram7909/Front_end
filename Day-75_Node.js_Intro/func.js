70798f56523c5f7a7d8c8f97640310773df91429
ram7909 <ramcharanmain07@gmail.com>
2024-07-16 16:42:54 +0530

    Node Intro

---
+++ Day-75_Node.js_Intro/func.js
@@ -0,0 +1,12 @@
+export const sum = (a,b) =>{
+    console.log("sum = ",a+b);
+}
+export const sub = (a,b) =>{
+    console.log("substract = ",a-b);
+}
+export const product = (a,b) =>{
+    console.log("Product = ",a*b);
+}
+export const division = (a,b) =>{
+    console.log("division = ",a/b);
+}
---
+++ Day-75_Node.js_Intro/index.js
@@ -0,0 +1,8 @@
+import { sum,sub,product,division } from "./func.js"
+
+sum(10,30)
+sub(30,20)
+product(10,10)
+division(90,30)
+
+console.log("Focus on your studies");
