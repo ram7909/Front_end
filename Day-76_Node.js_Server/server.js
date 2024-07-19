00d63f7cd12c9396c18704bb9499b321dfeafc5c
ram7909 <ramcharanmain07@gmail.com>
2024-07-19 16:41:45 +0530

    Express

--- Day-76_Node.js_Server/server.js
+++ Day-76_Node.js_Server/server.js
@@ -1,7 +1,20 @@
 import http from 'http'
-const server = http.createServer((req,res)=>{
-    console.log(req);
-    res.end("You Are Reuested For Something")
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        res.end("You are request for Home Route")
+    }
+    else if (req.url == '/samosa') {
+        res.end('We Dont Have Samosa Right Now')
+    }
+    else if (req.url == '/colddrink') {
+        res.end('you have to pay 100rs for that')
+    }
+    else if (req.url == '/profile') {
+        res.end('Welcome to profile')
+    }
+    else {
+        res.end('404 not found')
+    }
 });
-const port = 1000
-server.listen(port,()=>console.log(`Server is Running On Port ${port}`));
+const port = 1500;
+server.listen(port, () => console.log(`Server is Running On Port ${port}`));
