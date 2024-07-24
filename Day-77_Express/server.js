af531cb2b3428e5eb0c375da596ee8cfc395be6c
ram7909 <ramcharanmain07@gmail.com>
2024-07-24 16:51:56 +0530

    CRUD Operation in Express

--- Day-77_Express/server.js
+++ Day-77_Express/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import bodyParse from 'express'
+const app = express();
 
-const app = express()
+app.use(bodyParse.json())
+
 //CRUD Create Read Update Delete
 //Create => Post
 //Read => Get
@@ -14,17 +17,20 @@ const product = [
     { id: '3', productName: 'lava', price: 10000, os: 'android' }
 ]
 
-
-
 //Routing
 app.get('/', (req, res) => {
-    res.json({ name: 'Ramcharan', age: '20', salary: '20000', education: { 12: 'CBSE', 10: 'CBSE', UG: 'Holkar' }, message: 'You are requested for the / route', success: true })
-})
-app.get('/home', (req, res) => {
-    res.json({ message: 'Your request has been accepted',product, success: true });
-})
-app.get('/about', (req, res) => {
-    res.json({ institute: 'Volcanus', course: { courseName: 'java full stack', courseName: 'MERN Stack Developer' }, message: 'You are requested for the about route', success: true })
+    res.json({
+        name: 'Ramcharan',
+        age: '20',
+        salary: '20000',
+        education: {
+            12: 'CBSE',
+            10: 'CBSE',
+            UG: 'Holkar'
+        },
+        message: 'You are requested for the / route',
+        success: true
+    })
 })
 app.get('/product', (req, res) => {
     res.json({
@@ -33,6 +39,48 @@ app.get('/product', (req, res) => {
         success: true
     })
 })
+
+//get product by id (Dynamic Routing)
+app.get('/product/:id',(req,res) =>{
+    const id = req.params.id
+    const productDetail = product.filter((data) => data.id == id)
+    res.json({productDetail})
+})
+
+
+//add product by id
+app.post('/product/add', (req,res)=>{
+    // console.log(req.body);
+    product.push(req.body);
+    res.json({message:'Your Product Has Been Added Successfully',product})
+})
+
+//delete product by id
+app.delete('/product/:id', (req,res) => {
+    const id = req.params.id
+    const newProduct = product.filter((data) => data.id != id)
+    res.json({message:'Your Product Has Been Deleted',newProduct})
+})
+
+
+
+//update product by id
+app.put('/product/update',(req,res)=>{
+    const updatedProduct = req.body
+    for(let i=0;i<product.length;i++){
+        if(updatedProduct.id == product[i].id){
+            product[i].productName = updatedProduct.productName;
+            product[i].price = updatedProduct.price;
+            product[i].os = updatedProduct.os;
+            break;
+        }
+    }
+
+    res.json({message:'your product has been updated', product,success:true})
+
+})
+
+
 
 const port = 2000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
